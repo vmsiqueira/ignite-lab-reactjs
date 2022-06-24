3982883bbebd9d08b17811875ecafe5d6642b87c
Vitor Siqueira <vitormsiqueira@hotmail.com>
2022-06-24 18:38:51 -0300

    first commit at day 3

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ApolloProvider } from '@apollo/client';
+import { Router } from './Router';
+import { client } from './lib/apollo';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
+}
+
+export default App
